a4e7658257bf5c95aaa0af93748522e53bbc4bab
José Tapia <jose.tapia.rodriguez@hotmail.com>
2019-12-06 03:20:22 -0600

    va

--- almacen/settings.py
+++ almacen/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'users.middleware.AutoLogout',
+    #'users.middleware.AutoLogout',
 ]
 
 AUTO_LOGOUT_DELAY = 20
