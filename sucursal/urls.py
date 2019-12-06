94f5959fc9ac45ead9f50b00c9106c0fac3bf006
fredito1212 <fredito1212@hotmail.com>
2019-12-06 17:09:07 -0600

    corrección de ruta sucursal

--- sucursal/urls.py
+++ sucursal/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='sucursal_listado_url'),
     path('agregar', views.agregar, name="sucursal_agregar_url"),
     path('editar/<int:almacen_id>', views.editar, name="sucursal_editar_url"),
     path('baja/<int:almacen_id>', views.baja, name="sucursal_baja_url")
