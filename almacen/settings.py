0ae6cd333ff309cbf6f2b3cd0a5e52778eb5ce4b
fredito1212 <fredito1212@hotmail.com>
2019-12-06 00:13:54 -0600

    plantilla

--- almacen/settings.py
+++ almacen/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4',
+    'clear_cache',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +123,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/static/',
+]
 
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL='/'
