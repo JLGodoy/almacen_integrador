19c729bf7a1ccd6e56e10df3ddc6e119df9a1b17
Juan Luis Godoy <jlgodoyh@gmail.com>
2019-12-05 23:42:47 -0600

    ok

--- almacen/urls.py
+++ almacen/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('sucursal/', include('sucursal.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('register', views.register, name = 'register'),
-        path('change_password', views.change_password, name='password')
+    path('change_password', views.change_password, name='password')
+]
